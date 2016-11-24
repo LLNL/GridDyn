f71fff4998e98c81886397c44f2fcdcb62ff17b8
Philip Top <top1@llnl.gov>
2016-11-23 16:47:36 -0800

    change FNCS config generation

--- fncs/gridDynFNCS.cpp
+++ fncs/gridDynFNCS.cpp
@@ -113,9 +113,9 @@ int main (int argc, char *argv[])
   info.minTimeStep = 1;
   info.minTimeStepUnits = "us";
 
-  fncsRegister::instance()->makeZPLfile("gridDyn.zpl", info);
+  auto configString=fncsRegister::instance()->makeZPLConfig(info);
 
-  fncs::initialize("gridDyn.zpl");
+  fncs::initialize(configString);
 
   /* unless FNCS detects another simulator terminates early, the
   * this simulator will run from time step 0 to time step 9 */
