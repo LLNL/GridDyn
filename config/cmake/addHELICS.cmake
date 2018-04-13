27dd25650ffc519e450e9b4ca60ee636d578bb5d
Philip Top <phlptp@gmail.com>
2018-04-13 05:31:32 -0700

    updating addHelics

--- config/cmake/addHELICS.cmake
+++ config/cmake/addHELICS.cmake
@@ -1,7 +1,20 @@
 
+IF (MSVC)
+	set(HELICS_PATH_HINTS
+		C:/local/helics_1_0_1
+		C:/local/helics_1_0_0)
+ENDIF(MSVC)
 
 SHOW_VARIABLE(HELICS_INSTALL_PATH PATH "path to the helics installation" "${PROJECT_BINARY_DIR}/libs")
-	if (EXISTS ${HELICS_INSTALL_PATH}/cmake/HELICS/HELICSImport.cmake)
+Find_file(HELICS_IMPORT_FILE HELICSImport.cmake
+	HINTS ${HELICS_INSTALL_PATH PATH}
+		${HELICS_PATH_HINTS}
+	PATH_SUFFIXES cmake/HELICS
+				lib/cmake/HELICS)
+				
+	
+
+	if (EXISTS ${HELICS_IMPORT_FILE})
 		IF (AUTOBUILD_HELICS)
 			OPTION(FORCE_HELICS_REBUILD "force rebuild of helics" OFF)
 			IF(FORCE_HELICS_REBUILD)
@@ -12,13 +25,13 @@ SHOW_VARIABLE(HELICS_INSTALL_PATH PATH "path to the helics installation" "${PROJ
 			ENDIF(FORCE_HELICS_REBUILD)
 		ENDIF(AUTOBUILD_HELICS)
 		message(status "loading ${HELICS_INSTALL_PATH}/cmake/HELICS/HELICSImport.cmake")
-		include(${HELICS_INSTALL_PATH}/cmake/HELICS/HELICSImport.cmake)
+		include(${HELICS_INSTALL_PATH}/lib/cmake/HELICS/HELICSImport.cmake)
 	else()
 		message(status "couldn't find ${HELICS_INSTALL_PATH}/cmake/HELICS/HELICSImport.cmake")
 		OPTION(AUTOBUILD_HELICS "enable HELICS to automatically download and build" OFF)
 		IF(AUTOBUILD_HELICS)
 			include(buildHELICS)
 			build_helics()
-			include(${HELICS_INSTALL_PATH}/cmake/HELICS/HELICSImport.cmake)
+			include(${HELICS_INSTALL_PATH}/lib/cmake/HELICS/HELICSImport.cmake)
 		ENDIF(AUTOBUILD_HELICS)
 	endif()
