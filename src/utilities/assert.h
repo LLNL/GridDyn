a2e2a6358e7a01af62babdef2132d18b65b9ab46
Corey McNeish <mcneish1@llnl.gov>
2018-05-31 12:47:47 -0700

    Add assert.h
    (Probably don't actually want to use a stringstream here)
    Also fix the * in the header of timeSeries.hpp

--- src/utilities/assert.h
+++ src/utilities/assert.h
@@ -1,4 +1,14 @@
-// copyright header
+/*
+ * LLNS Copyright Start
+ * Copyright (c) 2014-2018, Lawrence Livermore National Security
+ * This work was performed under the auspices of the U.S. Department
+ * of Energy by Lawrence Livermore National Laboratory in part under
+ * Contract W-7405-Eng-48 and in part under Contract DE-AC52-07NA27344.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * All rights reserved.
+ * For details, see the LICENSE file.
+ * LLNS Copyright End
+ */
 
 #pragma once
 
@@ -8,19 +18,25 @@
 
 #else
 #include <exception>
+#include <sstream>
 
 class assert_exception : public std::exception
 {
   public:
     assert_exception(int line) : m_line(line);
-    virtual const char *what () const noexcept override { "assert() failed at line " << std::to_string(line) << std::endl; }
+    virtual const char *what () const noexcept override {
+        std::stringstream ss;
+        ss << "assert() failed at line "
+           << m_line
+           << std::endl;
+        return ss.str().c_str();
+    }
   private:
-    int line = -1;
+    int m_line = -1;
 }
 
 #define assert(condition) \
-if (!condition) {         \
-    throw assert_exception(__LINE__) \
-}
+if (!condition) \
+    throw assert_exception(__LINE__)
 
 #endif
