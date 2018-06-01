3bf661de31e5dded88aadae7d61912c47b81b65b
Ryan Mast <mast9@llnl.gov>
2018-06-01 15:41:20 -0700

    Reduce some of the output from lcov + coveralls

--- scripts/lcov-helper.sh
+++ scripts/lcov-helper.sh
@@ -12,7 +12,7 @@ if [[ "$1" ]]; then
             if [[ "$2" ]]; then
                 GCOV_TOOL=$2
             fi
-            lcov --gcov-tool $GCOV_TOOL --no-external --directory . --capture --output-file coverage.info > /dev/null
+            lcov --gcov-tool $GCOV_TOOL --no-external --directory . --capture --output-file coverage.info &> /dev/null
             lcov -a coverage.base -a coverage.info --output-file coverage.total > /dev/null
             lcov --remove coverage.total 'test/*' 'tests/*' 'ThirdParty/*' 'dependencies/*' '/usr/*' --output-file coverage.info.cleaned > /dev/null
             ;;
