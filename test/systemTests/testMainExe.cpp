6a0d3b50aea9892cd4c52cb5ee0fc46d926c3ed0
Philip Top <top1@llnl.gov>
2018-06-14 14:46:57 -0700

    update testMainexe with more output on errors to try to diagnose issues

--- test/systemTests/testMainExe.cpp
+++ test/systemTests/testMainExe.cpp
@@ -25,13 +25,18 @@ BOOST_AUTO_TEST_SUITE (mainexe_tests, * boost::unit_test::label("quick"))
 
 BOOST_AUTO_TEST_CASE (mainexe_test1)
 {
-	exeTestRunner mainExeRunner(GRIDDYNINSTALL_LOCATION, GRIDDYNMAIN_LOCATION, "griddynMain");
+	exeTestRunner mainExeRunner( GRIDDYNMAIN_LOCATION, GRIDDYNINSTALL_LOCATION, "griddynMain");
     if (mainExeRunner.isActive ())
     {
         // printf("exestring=%s\n",mainExeRunner.getExeString().c_str());
         auto out = mainExeRunner.runCaptureOutput ("--version");
         // printf("out=%s\n",out.c_str());
         BOOST_CHECK (out.compare (0, 15, "GridDyn version") == 0);
+        if (out.compare(0, 15, "GridDyn version") != 0)
+        {
+            printf("mismatch out string\n");
+            printf("out=%s\n", out.c_str());
+        }
     }
     else
     {
@@ -42,7 +47,7 @@ BOOST_AUTO_TEST_CASE (mainexe_test1)
 // test is in development
 BOOST_AUTO_TEST_CASE (cdf_readwrite_test)
 {
-	exeTestRunner mainExeRunner(GRIDDYNINSTALL_LOCATION, GRIDDYNMAIN_LOCATION, "griddynMain");
+	exeTestRunner mainExeRunner(GRIDDYNMAIN_LOCATION, GRIDDYNINSTALL_LOCATION,"griddynMain");
     if (mainExeRunner.isActive ())
     {
         std::string fileName = pFlow_test_directory + "test_powerflow3m9b2.xml";
