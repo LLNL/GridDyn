b75b7b1550b762a828623410267a1a3f5a038521
Ryan Mast <mast9@llnl.gov>
2018-06-18 15:39:26 -0700

    Use initial cache setting cmake scripts for sanitizers on Travis

---
+++ scripts/asan-cache.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_FLAGS "-fsanitizer=address -fno-omit-frame-pointer" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_CXX_FLAGS "-fsanitizer=address -fno-omit-frame-pointer" CACHE STRING "Flags used by the compiler during all build types.")
---
+++ scripts/tsan-cache.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_FLAGS "-fsanitizer=thread -fno-omit-frame-pointer" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_CXX_FLAGS "-fsanitizer=thread -fno-omit-frame-pointer" CACHE STRING "Flags used by the compiler during all build types.")
