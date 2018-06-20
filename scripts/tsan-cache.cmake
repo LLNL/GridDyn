32899e54b6fb264282e921971715c75350c734f3
Ryan Mast <mast9@llnl.gov>
2018-06-19 18:04:33 -0700

    Add -g and -O1 flags for sanitizer builds

--- scripts/tsan-cache.cmake
+++ scripts/tsan-cache.cmake
@@ -1,2 +1,2 @@
-set(CMAKE_C_FLAGS "-fsanitize=thread -fno-omit-frame-pointer" CACHE STRING "Flags used by the compiler during all build types.")
-set(CMAKE_CXX_FLAGS "-fsanitize=thread -fno-omit-frame-pointer" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_C_FLAGS "-fsanitize=thread -fno-omit-frame-pointer -g -O1" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_CXX_FLAGS "-fsanitize=thread -fno-omit-frame-pointer -g -O1" CACHE STRING "Flags used by the compiler during all build types.")
