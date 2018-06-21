18739d4ee45ba16ea5eac812dc02abdee5186a2b
Ryan Mast <nightlark@users.noreply.github.com>
2018-06-21 11:34:15 -0700

    Add Address/Memory Sanitizer and Valgrind builds (#20)
    * Changes to ctest labels and test execution for valgrind
    
    * Add ASan and MSan to daily builds
    
    * Enable Autobuilding SuiteSparse in addtion to KLU
    
    * Convert option flags from an array before using as an argument
    
    * Echo the actual cmake command run...
    
    * Use initial cache setting cmake scripts for sanitizers on Travis
    
    * Change sanitizer argument to sanitize -- hopefully last change needed
    
    * Change order of cmake arguments to make initial cache file appear first
    
    * Try using clang++ for CC and CXX -- Travis default clang version mix and matching might cause -fPIC issue with msan
    
    * Try clang 4 instead of 5
    
    * Add -g and -O1 flags for sanitizer builds
    
    * Remove no-omit-frame-pointer from tsan compilation flags
    
    * Run 179bus dynamic test with Valgrind in a daily Travis build
    
    * Output log file from running valgrind
    
    * Move coverage dashboard submission into the coverage helper script
    
    * Move logic for interaction between running tests with different sanitizers to a script
    
    * Output more information for LSAN failure
    
    * Disable leak detection with LSAN
    
    * Run ASan on unit  tests with LSan disabled.
    
    * Run unit tests with ASan
    
    * Export LSAN and ASAN options as env variables

--- scripts/lcov-helper.sh
+++ scripts/lcov-helper.sh
@@ -8,13 +8,47 @@ if [[ "$1" ]]; then
             lcov --no-external --capture --initial --directory . --output-file coverage.base > /dev/null
             ;;
         gather-coverage-info)
+            shift
             GCOV_TOOL=gcov
-            if [[ "$2" ]]; then
-                GCOV_TOOL=$2
-            fi
+            while [[ $# -gt 0 ]]
+            do
+                key="$1"
+                case $key in
+                    -g|--gcov)
+                        GCOV_TOOL="$2"
+                        shift
+                        shift
+                        ;;
+                    --coveralls)
+                        SUBMIT_COVERALLS=true
+                        shift
+                        ;;
+                    --codecov)
+                        SUBMIT_CODECOV=true
+                        shift
+                        ;;
+                    *)
+                        shift
+                        ;;
+            esac
+            done
+ 
+            # Get the coverage info from the test runs into a file
             lcov --gcov-tool $GCOV_TOOL --no-external --directory . --capture --output-file coverage.info &> /dev/null
+
+            # Combine the base coverage info with info from running programs
             lcov -a coverage.base -a coverage.info --output-file coverage.total > /dev/null
+
+            # Clean-up the coverage info
             lcov --remove coverage.total 'test/*' 'tests/*' 'ThirdParty/*' 'dependencies/*' '/usr/*' --output-file coverage.info.cleaned > /dev/null
+
+            # Submit coverage info to dashboardv
+            if [[ "$SUBMIT_COVERALLS" == "true" ]]; then
+                coveralls --gcov ${GCOV_TOOL} --lcov-file coverage.info.cleaned > /dev/null 
+            fi
+            if [[ "$SUBMIT_CODECOV" == "true" ]]; then
+                bash <(curl -s https://codecov.io/bash) -x ${GCOV_TOOL} > /dev/null
+            fi
             ;;
         *)
             echo "Usage: $@ setup-counters|gather-coverage-info"
