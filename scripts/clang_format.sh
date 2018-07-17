ee8848df894f9858bd043771710561ae351686da
Corey McNeish <mcneish1@llnl.gov>
2018-07-17 08:45:41 -0700

    Save updates from out of date branches

---
+++ scripts/clang_format.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# clang-tidy has segfaulted on me trying to do all the files at once; do them
+# a directory at a time
+# TODO parallelize this?
+for i in $(ls ../src); do
+    echo "Directory: $i"
+    files=$(find ../src/$i | grep -E '.cpp|.hpp')
+    for f in $files; do
+        clang-format -i $f
+    done
+done
--- scripts/clang_tidy.sh
+++ scripts/clang_tidy.sh
@@ -2,8 +2,10 @@
 # clang-tidy has segfaulted on me trying to do all the files at once; do them
 # a directory at a time
 # TODO parallelize this?
+filename=../tidy/$(basename $(pwd))_$(date +%m%d%y%H%M).tidy
+echo "Writing to $filename"
 for i in $(ls ../src); do
     files=$(find ../src/$i | grep .cpp)
     echo $files
-    clang-tidy $files >> src.tidy
+    clang-tidy $files >> $filename
 done
