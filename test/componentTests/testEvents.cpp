38d15b8df1a9bb96d52622335c1b72435d777b61
Ryan Mast <mast9@llnl.gov>
2018-08-27 13:22:31 -0700

    Add a unit test for loading event strings

--- test/componentTests/testEvents.cpp
+++ test/componentTests/testEvents.cpp
@@ -24,6 +24,22 @@ static std::string event_test_directory = std::string (GRIDDYN_TEST_DIRECTORY "/
 
 BOOST_FIXTURE_TEST_SUITE (event_tests, gridDynSimulationTestFixture)
 
+BOOST_AUTO_TEST_CASE (event_test_loadstring)
+{
+    std::string fileName = event_test_directory + "event_test_loadstring.xml";
+    auto gds = griddyn::readSimXMLFile(fileName);
+
+    griddyn::EventInfo gdEI;
+    gdEI.loadString("@2+3|LINK#1:param(MW)=13", gds->getRoot());
+
+    BOOST_CHECK(gdEI.targetObjs[0] != nullptr);
+    BOOST_CHECK_EQUAL(gdEI.fieldList[0], "param");
+    BOOST_CHECK_EQUAL(gdEI.value[0], 13);
+    BOOST_CHECK_EQUAL(gdEI.units[0], gridUnits::MW);
+    BOOST_CHECK_EQUAL(gdEI.time[0], griddyn::coreTime(2));
+    BOOST_CHECK_EQUAL(gdEI.period, griddyn::coreTime(3));
+}
+
 BOOST_AUTO_TEST_CASE (event_test1) {}
 BOOST_AUTO_TEST_CASE (event_test2) {}
 BOOST_AUTO_TEST_CASE (event_test3) {}
