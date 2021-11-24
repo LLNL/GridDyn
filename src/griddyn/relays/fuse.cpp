e5bca3f19c306a6b84537fb97048da205df2d686
David J. Gardner <gardner48@llnl.gov>
2021-11-24 11:50:17 -0800

    remove fuse debugging output

--- src/griddyn/relays/fuse.cpp
+++ src/griddyn/relays/fuse.cpp
@@ -316,8 +316,6 @@ namespace relays {
                         double resid[],
                         const solverMode& sMode)
     {
-        std::cout << "fuse::residual" << std::endl;
-
         if (useI2T) {
             auto offset = offsets.getDiffOffset(sMode);
             const double* dst = sD.dstate_dt + offset;
